32818b243cfdefa994eb75d6db64a1b95442d432
tq <2310307125@qq.com>
2022-05-31 09:19:17 +0800

    fff

---
+++ pmd.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('pmd') {
+            steps {
+                sh "mvn pmd:pmd"
+            }
+        }
+    }
+
+    post {
+        always {
+            pmd(canRunOnFailed: true, pattern: "**/target/pmd.xml")
+        }
+    }
+}
---
+++ src/main/java/org/example/ss/HHellwService.java
@@ -0,0 +1,17 @@
+package org.example.ss;
+
+/**
+ * @author tq
+ * @create 2022-05-31 9:10 AM
+ * @descritpion
+ */
+public class HHellwService {
+
+    public void sss(String val1, String val2) {
+
+    }
+
+    public String get(String val1) {
+        return null;
+    }
+}
